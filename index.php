82848dc4ad486c607dd13b57041d539f6a9c84f0
Theodore R. Smith <theodore@phpexperts.pro>
2014-08-07 11:17:33 -0500

    We're not ready for Blitz yet.

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-//header("Location: http://" . $_SERVER['HTTP_HOST'] . '/login.php');
-ob_start();
-
-require_once('views/LoginView.inc');
-$T = new LoginView('tpl/login.tpl');
-echo $T->parse();
-
+header("Location: http://" . $_SERVER['HTTP_HOST'] . '/login.php');
 ?>
