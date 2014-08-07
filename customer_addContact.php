5f128b562df3eb8c5d4ccb30fb68f38ad5312545
Theodore R. Smith <theodore@phpexperts.pro>
2014-08-07 11:17:32 -0500

    Update to 20080414 branch.

--- customer_addContact.php
+++ customer_addContact.php
@@ -9,11 +9,9 @@ if (!isset($_POST['RollupID']) || !isset($_POST['ContactFirstName']))
 }
 
 require_once('config.php');
-$db = getDB();
 
-mysql_connect($db['server'], $db['user'], $db['pass']);
-mysql_select_db($db['database']);
-print mysql_error();
+require_once('lib/db.php');
+db_connect();
 
 $q1s = sprintf('INSERT INTO tblcustomercontacts (RollupID, ContactFirstName, ContactLastName, ' .
                'ContactNumber, ContactNumberTypeID, ContactEmail, RecordDate) ' .
